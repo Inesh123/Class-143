0bc08aae28d5819ed9bc0df262e318c7c75ac3c5
Inesh123 <64844708+Inesh123@users.noreply.github.com>
2021-05-29 12:03:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/home';
+
+export default function App() {
+  return (
+    <HomeScreen/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
